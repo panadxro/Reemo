ee02b7054862cbf05758b3704141f0b80d3f01d6
Lucas Panadero <lucasn.panadero@gmail.com>
2025-02-27 22:44:20 -0300

    Update snap scroll

--- src/pages/Publications.vue
+++ src/pages/Publications.vue
@@ -57,17 +57,17 @@ export default {
 };
 </script>
 
-<template class="p-2.5 flex flex-col items-center">
+<template>
   <div class="bg-secondary-100 m-2.5 min-w-[368px] py-10 px-5">
     <Heading :type="1" class="m-6 text-center">Filtros</Heading>
   </div>
-  <section class="m-2.5 flex flex-col w-full overflow-hidden">
+  <section class="m-2.5 flex flex-col w-full gap-3 overflow-hidden">
     <div class="flex justify-between items-center">
-    <Heading :type="1" class="m-6 text-center">Autos disponibles</Heading>
+      <Heading :type="1" class="m-6 text-center">Autos disponibles</Heading>
     
-    <template v-if="loggedUser.id == null">
-      <router-link to="/Login"
-      class="gap-4 md:flex z-50 items-center justify-center bottom-0 right-0 m-8 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-hidden focus:ring-blue-300 font-medium rounded-full md:rounded-lg text-md px-2 md:px-4 py-2 text-center">
+      <template v-if="loggedUser.id == null">
+        <router-link to="/Login"
+        class="gap-4 md:flex items-center justify-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-hidden focus:ring-blue-300 font-medium rounded-full md:rounded-lg text-md px-2 md:px-4 py-2 text-center">
           <span class="hidden md:block">Publicar Vehículo</span>
           <AddIcon />
         </router-link>
@@ -75,7 +75,7 @@ export default {
 
       <template v-else>
         <router-link to="/Publish"
-          class="gap-4 md:flex items-center justify-center bottom-0 right-0 m-8 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-hidden focus:ring-blue-300 font-medium rounded-full md:rounded-lg text-md px-2 md:px-4 py-2 text-center">
+          class="gap-4 md:flex items-center justify-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-hidden focus:ring-blue-300 font-medium rounded-full md:rounded-lg text-md px-2 md:px-4 py-2 text-center">
           <span class="hidden md:block">Publicar Vehículo</span>
           <AddIcon />
         </router-link>
@@ -86,12 +86,12 @@ export default {
       <Loading role="status" />
       <span class="sr-only">Cargando...</span>
     </div>
-    <div v-else class="overflow-y-scroll">
+    <div v-else class="h-full overflow-auto">
       <div  class="grid justify-items-center gap-3 grid-cols-2">
         <div 
           v-for="(car, index) in cars" 
           :key="index"
-          class="rounded-2xl flex relative flex-col shadow-xs w-full overflow-hidden"
+          class="rounded-2xl flex relative flex-col shadow-xs w-full"
           >
         <CardCar :car="car" />
       </div>
