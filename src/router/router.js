f618ade4a9559df635fca0da08ec1a19ca4bbc6d
lucasn.panadero <lucasn.panadero@gmail.com>
2025-04-17 03:40:16 +0000

    Fix route redirection

--- src/router/router.js
+++ src/router/router.js
@@ -97,68 +97,24 @@ const router = createRouter({
   }
 });
 
-/* router.beforeEach(async (to) => {
-  const authStore = useAuthStore();
-  // Esperar que se resuelva el estado de autenticacion del usuario
-  if (!authStore.isInitialiazed) {
-    await new Promise((resolve) => {
-      const unsubscribe = authStore.$subscribe((mutation, state) => {
-        if (state.isInitialiazed) {
-          unsubscribe();
-          resolve();
-        }
-      });
-    });
-  }
-  // Si el usuario está logueado
-  if (authStore.isLoggedIn) {
-    // if (to.meta.isLogin) return "/" // Esto es dudoso "isLogin"
-    console.log("Hola" + to.meta.isLogin)
-    return true;
-  }
-  // Si no requiere auth, sigue adelante
-  if (!to.meta.needsAuth) return true;
-  // Si el usuario no esta logueado y requiere auth
-  if (to.meta.needsAdmin) {
-    const userStore = useUserStore();
-    await userStore.loadUserProfile(authStore.user.id);
-    if (to.meta.role && userStore.profileData.role !== to.meta.role) {
-      return {
-        path: "/",
-      };
-    }
-  }
-  return "/login";
-}); */
-
 router.beforeEach(async (to) => {
   const authStore = useAuthStore();
 
   // Si el usuario está logueado, permite la navegación
   if (authStore.isLoggedIn) {
-      return true;
+    return true;
   }
   // Si no esta logueado pero el authstore esta inicializado
   else {
-      // Si no requiere auth, continúa con getCurrentUser
-      if (!to.meta.needsAuth) {
-          return true;
-      }
-      // Si la ruta requiere autenticación, y el store esta inicializado, redirige a /login
-      if (to.meta.needsAuth && !authStore.isInitialiazed) {
-        
-          return { path: "/login", query: { redirect: to.fullPath } };
-      }
+    // Si no requiere auth
+    if (!to.meta.needsAuth) {
+      return true;
+    }
+    // Si la ruta requiere autenticación, y el store esta inicializado, redirige a /login
+    if (to.meta.needsAuth && !authStore.isLoggedIn) {
+      return { path: "/login", query: { redirect: to.fullPath } };
+    }
   }
-if (to.meta.needsAdmin) {
-  const userStore = useUserStore();
-  await userStore.loadUserProfile(authStore.user.id);
-  if (to.meta.role && userStore.profileData.role !== to.meta.role) {
-    return {
-      path: "/",
-    };
-  }
-}
 })
 
 export default router;
