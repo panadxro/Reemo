48f1d889efdc4d03879c98a75adcc2520ba312d9
lucasn.panadero <lucasn.panadero@gmail.com>
2025-04-20 00:46:43 +0000

    Fix Redirect User Logged

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { useAuthStore, useUserStore } from '@stores'
+import { useAuthStore, useUserStore } from '@stores';
+import { storeToRefs } from 'pinia';
 
 import Home from "../pages/Home.vue";
 import Login from "../pages/Login.vue";
@@ -98,22 +99,11 @@ const router = createRouter({
 });
 
 router.beforeEach(async (to) => {
-  const authStore = useAuthStore();
-  // Esperar que se resuelva el estado de autenticacion del usuario
-  if (!authStore.isInitialiazed) {
-    await new Promise((resolve) => {
-      const unsubscribe = authStore.$subscribe((mutation, state) => {
-        if (state.isInitialiazed) {
-          unsubscribe();
-          resolve();
-        }
-      });
-    });
-  }
-  // Si el usuario está logueado
-  if (authStore.isLoggedIn) {
-    if (to.meta.isLogin) return "/" // Esto es dudoso "isLogin"
+  const authSessionHistory = sessionStorage.getItem('auth_session_history');
+  const authSession = JSON.parse(authSessionHistory);
 
+  // Si el usuario está logueado, permite la navegación
+  if (authSession.isLoggedIn === true) {
     return true;
   }
   // Si no esta logueado pero el authstore esta inicializado
@@ -123,11 +113,10 @@ router.beforeEach(async (to) => {
       return true;
     }
     // Si la ruta requiere autenticación, y el store esta inicializado, redirige a /login
-    if (to.meta.needsAuth && isLoggedIn.value === false) {
-      // return { path: "/login", query: { redirect: to.fullPath } };
+    if (to.meta.needsAuth && authSession.isLoggedIn === false) {
+      return { path: "/login", query: { redirect: to.fullPath } };
     }
   }
-  return "/login";
-});
+})
 
 export default router;
--- src/services/user/core.js
+++ src/services/user/core.js
@@ -5,9 +5,24 @@ import { getDownloadURL } from 'firebase/storage';
 
 export async function createUserProfile( uid, email ) {
   const userRef = doc(db, 'users', uid);
-  await setDoc(userRef, {
+  const initialData = {
+    personalInfo: {
+      firstName: 'Desconocido',
+      lastName: '',
+      profilePhoto: '/src/assets/User.png',
+      username: 'Desconocido'
+    },
+    documents: {},
+    address: {},
+    paymentMethods: [],
+    agreements: {},
+    email: email,
+    role: 'user'
+  }
+    await setDoc(userRef, {
+    ...initialData,
+
     uid, 
-    email, 
     emailVerified: false, 
     profileCompleted: false, 
     role: 'user', 
@@ -48,10 +63,7 @@ export async function saveUserData(uid, data) {
     if (!uid) throw new Error("UID is required")
 
     const userRef = doc(db, 'users', uid);
-    const cleanData = {
-      ...data,
-      updateAt: serverTimestamp()
-    }
+
     await setDoc(userRef, {
       ...data,
       updateAt: serverTimestamp()
