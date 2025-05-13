866947d5f1fa94452306ab976a7dfc4e2cda8eff
GasipiValentino <valentinogasipi@gmail.com>
2025-05-13 18:48:59 -0300

    Card Car and Car List

--- src/components/organisms/my-cars/CardCar.vue
+++ src/components/organisms/my-cars/CardCar.vue
@@ -41,9 +41,10 @@ export default {
     setDefaultImage(event) {
       event.target.src = this.defaultCarImage;
     },
-    handleLike() {
-      console.log('Like');
-    }
+    handleLike(event) {
+      event.preventDefault();
+      this.$emit('like', this.car.id);
+    },
 
 /*     async handleDelete(id) {
   this.loading = true;
@@ -98,37 +99,60 @@ export default {
 </script>
 
 <template>
-  <div class="relative flex flex-col border-2 rounded-3xl border-secondary-100 p-4">
+  <div class="relative flex flex-col overflow-hidden border border-gray-200 rounded-2xl shadow-sm hover:shadow-md transition-all duration-300 bg-white">
     <button
       @click.stop="handleLike"
-      class="absolute top-4 right-4 z-10 p-2 rounded-full"
+      class="absolute top-3 right-3 z-10 p-2 bg-white/80 backdrop-blur-sm rounded-full shadow-sm hover:bg-white transition-all duration-300"
+      aria-label="Añadir a favoritos"
     >
       <Like />
     </button>
 
     <router-link 
       :to="{ name: 'CarDetails', params: { id: car.id } }" 
-      class="flex flex-col"
+      class="flex flex-col h-full"
     >
-      <div class="flex flex-row gap-2">
-        <Status :status="car.isAvailable ? 'validated' : 'not-validated'" />
-      </div>
-      <div class="flex justify-end">
-        <figure class="aspect-[21/9] h-24 overflow-hidden"> 
+      <div class="relative w-full overflow-hidden">
+        <div class="absolute top-3 left-3 z-10">
+          <Status :status="car.isAvailable ? 'validated' : 'not-validated'" />
+        </div>
+        
+        <div class="w-full aspect-[16/9] overflow-hidden">
           <img
-            class="rounded-xl object-center object-cover w-full h-full"
+            class="w-full h-full object-cover object-center transition-transform duration-700 hover:scale-105"
             :src="car.images && car.images.length > 0 ? car.images[0] : defaultCarImage" 
             @error="setDefaultImage"
             :alt="car.marca + ' ' + car.modelo" 
           />
-        </figure>
+        </div>
       </div>
-      <div class="flex flex-1 flex-row justify-between items-end">
-        <article class="flex flex-col align-end justify-between">
-          <p class="text-sm text-gray-500">{{ car.marca }}</p>
-          <Heading :type="4">{{ car.modelo }}</Heading>
-        </article>
-        <Heading :type="4">${{ car.precio }}/ hr</Heading>
+
+      <div class="flex flex-col p-4 flex-1">
+        <div class="mb-2">
+          <p class="text-sm font-medium text-primary-500">{{ car.marca }}</p>
+          <Heading :type="4" class="text-gray-900 font-bold line-clamp-1">{{ car.modelo }}</Heading>
+        </div>
+        
+        <div class="flex gap-3 mt-1 mb-3">
+          <div class="flex items-center text-xs text-gray-500">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
+            </svg>
+            {{ car.transmision || "Automático" }}
+          </div>
+          <div class="flex items-center text-xs text-gray-500">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+            </svg>
+            {{ car.year || "2023" }}
+          </div>
+        </div>
+        
+        <div class="flex justify-between items-center mt-auto pt-2 border-t border-gray-100">
+          <div class="flex items-center">
+            <Heading :type="4" class="text-primary-600 font-bold">${{ car.precio }}<span class="text-xs font-normal">/hr</span></Heading>
+          </div>
+        </div>
       </div>
     </router-link>
   </div>
--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -5,7 +5,6 @@ import { subscribeToNewPublication } from "../services/publication.js";
 
 import { updateCars, initAutocomplete, loadGoogleMaps } from "../services/google-maps.js";
 import { filterByPreferences } from "../services/filterService.js";
-import {reactive} from 'vue';
 
 import AddressInput from "@/components/organisms/google-maps/AddressInput.vue";
 
@@ -17,7 +16,6 @@ import Input from "../components/molecules/Input.vue";
 import Arrow from '@icons/Arrow.vue'
 import PriceRange from "../components/molecules/PriceRange.vue";
 import Checkbox from "../components/atoms/Checkbox.vue";
-
 
 export default {
   name: "Search",
@@ -35,13 +33,14 @@ export default {
       map: null,
       markers: [],
       loading: false,
-      chassisTypes: ["Sedan", "Van", "SUV", "Pickup", "Minivan", "Coupe"], 
-      selectedChassis: [], 
-      savedFilters: null, 
+      chassisTypes: ["Sedan", "Van", "SUV", "Pickup", "Minivan", "Coupe"],
+      selectedChassis: [],
+      savedFilters: null,
       optionsTransmission: ["Ambos", "Manual", "Automatico"],
       selectedTransmission: [],
+      showFilters: false, 
       filters: {
-        transmission: '', 
+        transmission: '',
         brand: '',
         model: '',
         minPrice: 20000,
@@ -55,9 +54,7 @@ export default {
       this.loading = true;
       try {
         this.cars = await getAvailableCars(this.loggedUser.id);
-
         this.filteredCars = this.cars;
-
       } catch (error) {
         console.error("Error al buscar autos:", error);
       } finally {
@@ -75,69 +72,62 @@ export default {
     goToCarDetails(carId) {
       this.$router.push({ name: "CarDetails", params: { id: carId } });
     },
-
     handlePlaceSelected({ formattedAddress, location }) {
       this.searchQuery = formattedAddress;
       this.searchLocation = location;
       this.applyFilters();
     },
-
     applyFilters() {
       let carsToFilter = [];
 
-      if ( this.searchLocation && this.searchLocation.lat && this.searchLocation.lng) {
+      if (this.searchLocation && this.searchLocation.lat && this.searchLocation.lng) {
         carsToFilter = updateCars(this.cars, this.searchLocation, this.map);
       } else {
         console.warn("⚠️ No se aplicó filtro por ubicación");
-        // usamos todos los autos disponibles
-        carsToFilter = this.cars; 
+        carsToFilter = this.cars;
       }
 
       // luego filtramos por preferencias
       this.filteredCars = filterByPreferences(carsToFilter, this.filters);
-
       this.saveFiltersToLocalStorage();
-
-      // console.log("filtros aplicados:", this.filters);
-      // console.log("autos filtrados:", this.filteredCars);
+      
+      if (window.innerWidth < 1000) {
+        this.showFilters = false;
+      }
     },
-
-    saveFiltersToLocalStorage(){
+    saveFiltersToLocalStorage() {
       localStorage.setItem('filters', JSON.stringify(this.filters));
     },
-
-    loadFiltersFromLocalStorage(){
+    loadFiltersFromLocalStorage() {
       const savedFilters = localStorage.getItem('filters');
-      if(savedFilters){
+      if (savedFilters) {
         this.filters = JSON.parse(savedFilters);
         this.applyFilters();
-      } 
+      }
     },
-
-
-    // resetar los filtros
-    resetFilters(){
+    resetFilters() {
       this.filters = {
-        minPrice : 20000,
-        maxPrice : 100000,
-        brand : "",
-        model : "",
-        chassis : [],
-        transmission : "",
+        minPrice: 20000,
+        maxPrice: 100000,
+        brand: "",
+        model: "",
+        chassis: [],
+        transmission: "",
       };
       this.applyFilters();
-      localStorage.removeItem('filters')
+      localStorage.removeItem('filters');
     },
-    
+    toggleFilters() {
+      this.showFilters = !this.showFilters;
+    },
   },
   async mounted() {
-    
     await loadGoogleMaps();
     initAutocomplete('searchInput', this.handlePlaceSelected);
 
     const savedFilters = localStorage.getItem("filters");
-    if(savedFilters){
-      this.filters = JSON.parse(savedFilters)
+    if (savedFilters) {
+      this.filters = JSON.parse(savedFilters);
     }
 
     subscribeToAuthState((newUserData) => {
@@ -147,7 +137,14 @@ export default {
 
     subscribeToNewPublication((newCars) => {
       this.cars = newCars;
-      // this.applyFilters();
+    });
+    
+    // Establecer el estado de los filtros según el tamaño de pantalla
+    this.showFilters = window.innerWidth >= 1000;
+    
+    // Listener para cambios de ventana
+    window.addEventListener('resize', () => {
+      this.showFilters = window.innerWidth >= 1000;
     });
   },
   watch: {
@@ -161,12 +158,140 @@ export default {
 </script>
 
 <template>
-  <section class="parent w-full">
+  <section class="w-full h-full relative">
+    <!-- Botón para mostrar/ocultar filtros en móvil -->
+    <button 
+      @click="toggleFilters" 
+      class="md:hidden fixed bottom-5 right-5 bg-primary-500 text-white p-3 rounded-full shadow-lg z-50"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+      </svg>
+    </button>
 
-    <div class="filter w-full m-2.5 min-w-[368px] rounded-[40px] px-5 flex flex-col gap-6">
+    <div class="flex flex-col md:flex-row w-full h-full">
+      <div 
+        :class="[
+          'transition-all duration-300 overflow-y-auto',
+          showFilters ? 'fixed md:relative inset-0 z-40 bg-white/95 md:bg-transparent' : 'hidden md:block',
+          'md:w-1/4 lg:w-1/4 xl:w-1/5 md:min-w-[300px] p-4'
+        ]"
+      >
+        <!-- <button 
+          @click="toggleFilters" 
+          class="absolute top-4 right-4 md:hidden text-gray-600 hover:text-gray-900"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button> -->
+
+        
+
+        <!-- Panel de filtros -->
+        <div class="bg-white border-secondary-100 border-2 rounded-2xl p-4 w-full h-auto">
+          <div class="flex items-center justify-between mb-4">
+            <h2 class="text-xl font-bold">Filtros</h2>
+            <div class="flex gap-2">
+              <button class="text-sm text-[#0ba5ec] hover:underline cursor-pointer" @click="resetFilters">Reset</button>
+              <button class="text-sm bg-[#0ba5ec] text-white px-4 py-1.5 rounded-full shadow-sm hover:bg-[#0998d2] transition-all cursor-pointer" @click="applyFilters">
+                Guardar
+              </button>
+            </div>
+          </div>
+
+          <!-- rango de precio -->
+          <div class="mb-4">
+            <Heading :type="3" class="small">Rango de precio</Heading>
+            <div class="mt-2">
+              <PriceRange :min="20000" :max="100000" v-model="filters" />
+            </div>
+          </div>
+
+          <!-- chasis -->
+          <div class="flex flex-col gap-2 mt-5">
+            <Heading :type="3" class="small">Chasis</Heading>
+            <div class="grid grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-2 mt-2 text-sm">
+              <Checkbox 
+                v-for="chassis in chassisTypes" 
+                :key="chassis" 
+                :id="chassis.toLowerCase()"
+                :name="chassis.toLowerCase()" 
+                :label="chassis" 
+                labelPosition="right" 
+                class="text-deep-blue-900"
+                v-model="filters.chassis" 
+                :value="chassis" 
+              />
+            </div>
+          </div>
+
+          <!-- marca -->
+          <div class="flex flex-col gap-2 mt-5">
+            <Heading :type="3" class="small">Marca del vehículo</Heading>
+            <div class="flex flex-col sm:flex-row gap-2 mt-2 text-sm">
+              <!-- Marca -->
+              <select class="w-full sm:w-1/2 rounded-xl border border-gray-300 p-2 focus:ring-sky-400" v-model="filters.brand">
+                <option value="">Marca</option>
+                <option value="Honda">Honda</option>
+                <option value="Toyota">Toyota</option>
+                <option value="Ford">Ford</option>
+                <option value="Chevrolet">Chevrolet</option>
+                <option value="Volkswagen">Volkswagen</option>
+              </select>
+
+              <!-- Modelo -->
+              <select 
+                class="w-full sm:w-1/2 rounded-xl border border-gray-300 p-2" 
+                v-model="filters.model"
+                :disabled="!filters.brand" 
+                :class="{ 'text-gray-400': !filters.brand }"
+              >
+                <option value="">Modelo</option>
+                <option v-if="filters.brand === 'Honda'">Civic</option>
+                <option v-if="filters.brand === 'Honda'">CR-V</option>
+                <option v-if="filters.brand === 'Toyota'">Corolla</option>
+                <option v-if="filters.brand === 'Toyota'">Yaris</option>
+                <option v-if="filters.brand === 'Ford'">Focus</option>
+                <option v-if="filters.brand === 'Ford'">Fiesta</option>
+              </select>
+            </div>
+          </div>
+
+          <!-- transmisión -->
+          <div class="flex flex-col gap-2 mt-5">
+            <label class="text-sm font-medium">Transmisión</label>
+            <div class="flex flex-wrap gap-2 mt-2">
+              <button 
+                v-for="option in optionsTransmission" 
+                :key="option" 
+                @click="filters.transmission = option === 'Ambos' ? '' : option" 
+                :class="[
+                  'cursor-pointer px-3 py-1 text-sm rounded-full border',
+                  filters.transmission === (option === 'Ambos' ? '' : option)
+                    ? 'bg-[#e6faff] border-[#0ba5ec] text-[#0ba5ec]'
+                    : 'bg-white border-gray-300 text-black'
+                ]"
+              >
+                {{ option }}
+              </button>
+            </div>
+          </div>
+
+          <!-- Mostrar disponibles -->
+          <div class="flex items-center justify-between mt-5">
+            <span class="font-medium">Mostrar sólo disponibles</span>
+            <label class="relative inline-flex items-center cursor-pointer">
+              <input type="checkbox" value="" class="sr-only peer">
+              <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:bg-blue-600"></div>
+              <div class="absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition peer-checked:translate-x-5"></div>
+            </label>
+          </div>
+        </div>
+      </div>
+
       <!-- Buscador -->
-      <div
-        class=" bg-white/70 rounded-full flex items-center px-4 py-2 w-full border border-gray-300 focus-within:ring-2 focus-within:ring-primary-500 z-50">
+      <div class="bg-white/70 rounded-full flex items-center px-4 py-2 w-full border border-gray-300 focus-within:ring-2 focus-within:ring-primary-500 mb-2">
         <svg class="w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
           stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -175,160 +300,32 @@ export default {
         <input type="text" id="searchInput" placeholder="Buscar un auto..."
           class="bg-transparent outline-none text-gray-700 w-full pl-2 placeholder-gray-400">
       </div>
-     
-
-
-      <div class="bg-white border-secondary-100 border-2 rounded-2xl p-4 w-full h-full overflow-hidden">
-        <!-- <div class="flex justify-between items-center mb-4">
-          <h2 class="text-lg font-semibold">Filtros</h2>
-          <button class="text-[#0ba5ec] text-sm cursor-pointer">Reset</button>
-        </div> -->
-
-        <div class="flex items-center justify-between mb-4">
-          <h2 class="text-xl font-bold">Filtros</h2>
-          <div class="flex gap-2">
-            <button class="text-sm text-[#0ba5ec] hover:underline cursor-pointer" @click="resetFilters">Reset</button>
-
-            <button class="text-sm bg-[#0ba5ec] text-white px-4 py-1.5 rounded-full shadow-sm hover:bg-[#0998d2] transition-all cursor-pointer" @click="applyFilters">
-              Guardar
-            </button>
-          </div>
+      <section class="w-full md:w-3/4 lg:w-3/4 xl:w-4/5 p-4 overflow-hidden flex flex-col h-full">
+        <div class="flex justify-between items-center mb-4">
+          <Heading :type="1" class="text-xl md:text-2xl">Autos disponibles</Heading>
         </div>
 
-
-        <!-- rango de precio -->
-        <div class="mb-4">
-          <Heading :type="3" class="small">Rango de precio</Heading>
-          <div class="mt-2">
-            <PriceRange :min="20000" :max="100000" v-model="filters"/>
-          </div>
+        <p v-if="filteredCars.length == 0 && !loading" class="text-lg text-red-700 font-bold pt-4">
+          No se encontraron autos con esas características
+        </p>
+        
+        <div v-if="loading" class="flex items-center justify-center w-full py-8">
+          <Loading role="status" />
+          <span class="sr-only">Cargando...</span>
         </div>
 
-        <!-- chasis -->
-        <div class="flex flex-col gap-2 mt-5">
-          <Heading :type="3" class="small">Chasis</Heading>
-          <div class="grid grid-cols-3 gap-2 mt-2 text-sm">
-            <Checkbox v-for="chassis in chassisTypes" :key="chassis" :id="chassis.toLowerCase()"
-              :name="chassis.toLowerCase()" :label="chassis" labelPosition="right" class="text-deep-blue-900 "
-              v-model="filters.chassis" :value="chassis" />
-          </div>
-        </div>
-
-        <!-- marca -->
-        <div class="flex flex-col gap-2 mt-5">
-          <Heading :type="3" class="small">Marca del vehículo</Heading>
-          <div class="flex gap-2 mt-2 text-sm">
-
-            <!-- Marca -->
-            <select class="w-1/2 rounded-xl border border-gray-300 p-2 focus:ring-sky-400" v-model="filters.brand">
-              <option value="">Marca</option>
-              <option value="Honda">Honda</option>
-              <option value="Toyota">Toyota</option>
-              <option value="Ford">Ford</option>
-              <option value="Chevrolet">Chevrolet</option>
-              <option value="Volkswagen">Volkswagen</option>
-            </select>
-
-            <!-- Modelo -->
-            <select class="w-1/2 rounded-xl border border-gray-300 p-2" v-model="filters.model"
-              :disabled="!filters.brand" :class="{ 'text-gray-400': !filters.brand }">
-              <option value="">Modelo</option>
-              <option v-if="filters.brand === 'Honda'">Civic</option>
-              <option v-if="filters.brand === 'Honda'">CR-V</option>
-              <option v-if="filters.brand === 'Toyota'">Corolla</option>
-              <option v-if="filters.brand === 'Toyota'">Yaris</option>
-              <option v-if="filters.brand === 'Ford'">Focus</option>
-              <option v-if="filters.brand === 'Ford'">Fiesta</option>
-
-            </select>
-
-          </div>
-
-          <!-- <div class="text-xs text-[#0ba5ec] mt-1 cursor-pointer">Más Vehículos ↓</div> -->
-        </div>
-
-        <!-- transmisión -->
-        <div class="flex flex-col gap-2 mt-5">
-          <label class="text-sm font-medium">Transmisión</label>
-          <div class="flex gap-2 mt-2">
-
-            <button v-for="option in optionsTransmission" :key="option" @click="filters.transmission = option === 'Ambos' ? '' : option" :class="[
-              'cursor-pointer px-3 py-1 text-sm rounded-full border',
-              filters.transmission === (option === 'Ambos' ? '' : option)
-                ? 'bg-[#e6faff] border-[#0ba5ec] text-[#0ba5ec]'
-                : 'bg-white border-gray-300 text-black'
-            ]">
-              {{ option }}
-            </button>
-          </div>
-        </div>
-
-        <!-- Mostrar disponibles -->
-        <div class="flex items-center justify-between">
-          <span class="font-medium">Mostrar sólo disponibles</span>
-          <label class="relative inline-flex items-center cursor-pointer">
-            <input type="checkbox" value="" class="sr-only peer">
-            <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:bg-blue-600"></div>
-            <div class="absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition peer-checked:translate-x-5">
+        <div v-else class="flex-1 overflow-y-auto">
+          <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+            <div 
+              v-for="(car, index) in filteredCars" 
+              :key="car.id"
+              class="rounded-2xl flex relative flex-col shadow-xs w-full"
+            >
+              <CardCar :car="car" />
             </div>
-          </label>
-        </div>
-
-      </div>
-    </div>
-
-
-
-    <section class="explore m-2.5 flex flex-col w-full gap-3 overflow-hidden">
-      <div class="flex justify-between items-center">
-        <Heading :type="1" class="m-6 text-center">Autos disponibles</Heading>
-
-        <template v-if="loggedUser.id == null">
-          <router-link to="/login"
-            class="gap-4 md:flex items-center justify-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-hidden focus:ring-blue-300 font-medium rounded-full md:rounded-lg text-md px-2 md:px-4 py-2 text-center">
-            <span class="hidden md:block">Publicar Vehículo</span>
-            <AddIcon />
-          </router-link>
-        </template>
-
-        <template v-else>
-          <router-link to="/publish"
-            class="gap-4 md:flex items-center justify-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-hidden focus:ring-blue-300 font-medium rounded-full md:rounded-lg text-md px-2 md:px-4 py-2 text-center">
-            <span class="hidden md:block">Publicar Vehículo</span>
-            <AddIcon />
-          </router-link>
-        </template>
-      </div>
-      <div v-if="loading" class="flex items-center justify-center w-fit mx-auto bg-gray-50">
-        <Loading role="status" />
-        <span class="sr-only">Cargando...</span>
-      </div>
-
-      <div v-else class="h-full overflow-auto">
-        <div class="grid justify-items-center gap-3 grid-cols-2">
-          <div v-for="(car, index) in filteredCars" :key="car.id"
-            class="rounded-2xl flex relative flex-col shadow-xs w-full">
-            <CardCar :car="car" />
           </div>
         </div>
-      </div>
-
-
-
-
-    </section>
+      </section>
+    </div>
   </section>
 </template>
-
-<style>
-  .parent {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-  }
-
-  .filter { grid-area: 1 / 1 / 3 / 2; }
-  .explore { grid-area: 1 / 2 / 3 / 4; }
-</style>
-
-
-
