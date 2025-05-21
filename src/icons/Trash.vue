e3704ea8c9d5051cf6fd4b9b04478b9ee43e7031
GasipiValentino <valentino.gasipi@davinci.edu.ar>
2025-05-20 23:00:47 -0300

    Payment Methods 1/2

---
+++ src/icons/Trash.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "Trash",
+};
+</script>
+
+<template>
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      viewBox="0 0 24 24" 
+      fill="none" 
+      stroke="currentColor" 
+      stroke-width="2" 
+      stroke-linecap="round" 
+      stroke-linejoin="round"
+      class="lucide lucide-trash-2"
+    >
+      <path d="m3 6 3 12c0 .6.4 1 1 1h10c.6 0 1-.4 1-1L21 6"/>
+      <path d="M8 6V4c0-.6.4-1 1-1h6c.6 0 1 .4 1 1v2"/>
+      <line x1="10" x2="10" y1="11" y2="17"/>
+      <line x1="14" x2="14" y1="11" y2="17"/>
+    </svg>
+  </template>
