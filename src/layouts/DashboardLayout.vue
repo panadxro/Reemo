7322363a4a0edf1138654b5c4f79a410036e57ad
Lucas Panadero <109440573+panadxro@users.noreply.github.com>
2025-06-13 19:39:16 -0300

    refactor: Update User icon import and clean up Dashboard layout

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,17 +1,12 @@
 <script setup>
-import Sidebar from '@/components/Sidebar.vue';
-import NavbarApp from '@/components/NavbarApp.vue';
+import Sidebar from '@/components/Sidebar.vue'
 </script>
 
 <template>
   <div class="snap-y snap-mandatory relative w-full h-screen overflow-auto">
-    <!-- snap-start -->
-    <main class="flex flex-row min-h-screen max-h-screen p-2.5 relative bg-white 2xl:rounded-[40px]">
-      <Sidebar />      
-      <NavbarApp />
-      <div class="mt-16 md:mt-0 w-full">
-        <slot />
-      </div>
+    <main class="flex flex-row min-h-screen max-h-screen p-2.5 snap-start relative bg-white 2xl:rounded-[40px]">
+      <Sidebar />
+      <slot />
     </main>
   </div>
 </template>
